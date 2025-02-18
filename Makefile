0322cdc13244bc17689d0edf97843a49626ea7c9
Aditya Gautam <adityascottish27@gmail.com>
2025-02-18 20:39:42 +0530

    I have lost faith in SDL and have moved back to Xlib

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 target: something
 
 something: src/main.c
-	gcc -Wall src/main.c -o bin/main -lSDL2
+	gcc -Wall src/main.c -o bin/main -lX11 -lXtst
 
 run: bin/main
 	./bin/main
--- src/main.c
+++ src/main.c
@@ -1,14 +1,33 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_mouse.h>
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/XTest.h>
+#include <X11/keysymdef.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
 
-  SDL_Init(SDL_INIT_EVENTS);
+  Display *display = XOpenDisplay(NULL);
+  int screen = DefaultScreen(display);
+  Window root_window = RootWindow(display, screen);
 
-  SDL_WarpMouseGlobal(200, 200);
+  XWarpPointer(display, None, root_window, 0, 0, 0, 0, 200, 200);
+  XFlush(display);
 
-  SDL_Quit();
+  KeyCode key_windows = XKeysymToKeycode(display, XK_Super_L);
+  KeyCode key_2 = XKeysymToKeycode(display, XK_2);
+
+  XTestFakeKeyEvent(display, key_windows, True, CurrentTime);
+  XTestFakeKeyEvent(display, key_2, True, CurrentTime);
+  XFlush(display);
+
+  sleep(0.1);
+
+  XTestFakeKeyEvent(display, key_2, False, CurrentTime);
+  XTestFakeKeyEvent(display, key_windows, False, CurrentTime);
+  XFlush(display);
+
+  XCloseDisplay(display);
 
   return 0;
 }
