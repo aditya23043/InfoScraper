c5a2994b4ad12e0a3e1d767637c69940ff3d5de3
Aditya Gautam <adityascottish27@gmail.com>
2025-03-05 15:02:07 +0530

    fixed mem leak and download working now

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <X11/keysymdef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 /* TODO: INITIAL CONFIG
@@ -16,13 +17,18 @@ void sleep_notify(int seconds, char *msg);
 
 int main(int argc, char **argv) {
 
+  if (argc != 2) {
+    printf("Usage: ./<bin> <prompt>");
+    exit(-1);
+  }
+
   Display *display;
   Window rootWindow;
 
   display = XOpenDisplay(NULL);
   rootWindow = XDefaultRootWindow(display);
 
-  char *browser_command = malloc(sizeof(char) * 256);
+  char browser_command[256];
   snprintf(browser_command, 256, "%s --new-window https://suno.com/create",
            check_browser());
   printf("%s", browser_command);
@@ -30,10 +36,63 @@ int main(int argc, char **argv) {
 
   sleep_notify(10, "Opening browser...");
 
+  // focusing on text box
   for (int i = 0; i < 22; i++) { /* NOTE hardcoded value */
     key_press(display, XKeysymToKeycode(display, XK_Tab));
     sleep(0.1);
   }
+
+  /* ---- Prompt Input ---- */
+
+  char *prompt = argv[1];
+
+  sleep(1);
+
+  // enter text
+  for (int i = 0; i < strlen(prompt); i++) {
+    KeyCode key_code;
+    if (prompt[i] == ' ') {
+      key_code = XKeysymToKeycode(display, XK_space);
+    } else {
+      char key_code_str[256];
+      snprintf(key_code_str, sizeof(key_code_str), "%c", prompt[i]);
+      key_code = XKeysymToKeycode(display, XStringToKeysym(key_code_str));
+    }
+
+    key_press(display, key_code);
+  }
+
+  // focus on create button
+  for (int i = 0; i < 4; i++) { /* NOTE hardcoded value */
+    key_press(display, XKeysymToKeycode(display, XK_Tab));
+    sleep(0.1);
+  }
+  key_press(display, XKeysymToKeycode(display, XK_Return));
+
+  // wait for song to generate
+  sleep_notify(40, "Generating song...");
+
+  // focus on three dot for downloading song
+  for (int i = 0; i < 18; i++) { /* NOTE hardcoded value */
+    key_press(display, XKeysymToKeycode(display, XK_Tab));
+    sleep(0.1);
+  }
+  key_press(display, XKeysymToKeycode(display, XK_Return));
+  sleep(2);
+
+  for (int i = 0; i < 5; i++) { /* NOTE hardcoded value */
+    key_press(display, XKeysymToKeycode(display, XK_Down));
+    sleep(1);
+  }
+  sleep(1);
+  key_press(display, XKeysymToKeycode(display, XK_Return));
+  sleep(1);
+  key_press(display, XKeysymToKeycode(display, XK_Return));
+
+  // now wait for the "where to download" prompt to come
+  /* sleep_notify(30, "Downloading..."); */
+
+  /* key_press(display, XKeysymToKeycode(display, XK_Return)); */
 
   XFlush(display);
   XCloseDisplay(display);
@@ -94,9 +153,9 @@ void sleep_notify(int seconds, char *msg) {
     char command[256];
     snprintf(command, sizeof(command), "notify-send '%s'", msg);
     system(command);
+    char cmd[256];
     while (seconds--) {
-      char *cmd = malloc(sizeof(char) * 256);
-      snprintf(cmd, 256, "notify-send %d -t 1000", seconds + 1);
+      snprintf(cmd, sizeof(cmd), "notify-send %d -t 1000", seconds + 1);
       system(cmd);
       sleep(1);
     }
