e0f19b107363c34354869df1b5067ab7d1bc4f48
Aditya Gautam <adityascottish27@gmail.com>
2025-03-16 22:09:27 +0530

    wip. commited to work on AIO

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import puppeteer from "puppeteer";
 
 const url = "https://suno.com/create";
-const browser = puppeteer.launch();
 
 const main = async () => {
-	const browser = await puppeteer.launch();
-	await browser.close();
-}
+  const browser = await puppeteer.launch({
+    headless: false,
+    browser: "firefox",
+    args: [
+      // "--disable-notifications",
+      // "--disable-features=BlockThirdPartyCookies",
+      "--profile ~/.mozilla/firefox/uqt3lbms.default-release/"
+    ],
+  });
+
+  const page = (await browser.pages()).at(0);
+  await page.goto(url);
+
+  await page.waitForSelector('textarea.custom-textarea');
+  await page.type('textarea.custom-textarea', "something");
+
+  await new Promise(resolve => setTimeout(resolve, 5000));
+
+  await browser.close();
+};
+
+main();
