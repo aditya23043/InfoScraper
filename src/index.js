e05373c87904f6b8132466f35e9e9d2dd1333694
Aditya Gautam <adityascottish27@gmail.com>
2025-03-19 23:58:45 +0530

    captcha detection done

--- src/index.js
+++ src/index.js
@@ -2,18 +2,6 @@ import puppeteer from "puppeteer";
 
 const url = "https://suno.com/create";
 
-async function waitForGenerationCompletion(page) {
-  await page.waitForFunction(
-    () => {
-      return (
-        document.querySelector("svg.animate-spin.text-white\\/75.w-8.h-8") ===
-        null
-      );
-    },
-    { timeout: 60000 }
-  );
-}
-
 const main = async () => {
   const browser = await puppeteer.launch({
     headless: false,
@@ -34,15 +22,37 @@ const main = async () => {
   await page.waitForSelector('button[aria-label="Create"]');
   await page.click('button[aria-label="Create"]');
 
+  try {
 
-  await new Promise((resolve) => setTimeout(resolve, 30000));
-  await waitForGenerationCompletion(page);
+    await page.waitForFunction(() => {
+      return document.querySelector('iframe[title="Main content of the hCaptcha challenge"]') !== null;
+    }, {timeout: 5000});
 
-  console.log("Generated!");
+    // await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log("Captcha found!");
+
+  } catch (error) {
+
+    console.log("Captcha not found withing time limit!");
+    await page.waitForFunction(
+      () => {
+        return (
+          document.querySelector("svg.animate-spin.text-white\\/75.w-8.h-8") ===
+          null
+        );
+      },
+      { timeout: 60000 }
+    );
+
+    console.log("Generated!");
+  }
 
   await browser.close();
+
 };
 
 main();
 
 // <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" class="animate-spin text-white/75 w-8 h-8"><path d="M12 2c5.52 0 10 4.48 10 10s-4.48 10-10 10-10-4.48-10-10c0-.41.34-.75.75-.75s.75.34.75.75c0 4.41 3.59 8 8 8s8-3.59 8-8-3.59-8-8-8c-.41 0-.75-.34-.75-.75S11.59 2 12 2z"></path></svg>
+// challenge-container : captcha
+// iframe title=Main content of the hCaptcha challenge
