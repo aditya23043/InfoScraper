6c7f71d71b43a11f2255306d2d82f04fda7104e7
Aditya Gautam <adityascottish27@gmail.com>
2025-04-14 21:44:04 +0530

    added windows compatibility

--- src/riffusion.py
+++ src/riffusion.py
@@ -4,12 +4,20 @@ import time
 import sys
 import pytesseract
 from PIL import Image
+import platform
+from datetime import datetime
+
+now = datetime.now()
 
 def foo(prompt):
 
     temp = []
 
     pyautogui.click(500, 100)
+    time.sleep(0.1)
+    pyautogui.click(437, 112)
+    time.sleep(0.1)
+    pyautogui.click(437, 170)
     time.sleep(0.1)
     pyautogui.hotkey("ctrl", "a")
     pyautogui.press("backspace")
@@ -18,9 +26,14 @@ def foo(prompt):
     time.sleep(2)
 
     while(True):
-        os.system("scrot -o /tmp/screen.png")
-        screenshot = Image.open("/tmp/screen.png")
-        text = pytesseract.image_to_string(screenshot)
+        text = ""
+        if(platform.system() == "Linux"):
+            os.system("scrot -o /tmp/screen.png")
+            screenshot = Image.open("/tmp/screen.png")
+            text = pytesseract.image_to_string(screenshot)
+        elif(platform.system() == "Windows"):
+            pyautogui.screenshot()
+            text = pytesseract.image_to_string(screenshot)
 
         if 'content guidelines' in text:
             with open(sys.argv[1], 'r') as f:
@@ -33,6 +46,7 @@ def foo(prompt):
                     elif line == prompt:
                         print("line removed!")
             print("Prompt didn't meet content guidelines: ", prompt)
+            return;
         elif 'Generating' in text:
             time.sleep(1)
         else:
@@ -60,9 +74,12 @@ def foo(prompt):
             if line != prompt:
                 f.write(line)
 
+    with open(sys.argv[2], 'a') as f:
+        f.write("Prompt: " + prompt + "\t(" + str(now.day) + "/" + str(now.month) + "/" + str(now.year) + " | " + str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + "\n")
 
-if (len(sys.argv) != 2):
-    print("Usage: python riffusion.py <prompts file>")
+
+if (len(sys.argv) != 3):
+    print("Usage: python riffusion.py <prompts file> <output log file>")
     exit(-1)
 
 prompts = []
@@ -72,9 +89,14 @@ with open(sys.argv[1], 'r') as f:
 os.system("firefox --new-window https://riffusion.com/library/my-songs");
 
 while(True):
-    os.system("scrot -o /tmp/screen.png")
-    screenshot = Image.open("/tmp/screen.png")
-    text = pytesseract.image_to_string(screenshot)
+    text = ""
+    if(platform.system() == "Linux"):
+        os.system("scrot -o /tmp/screen.png")
+        screenshot = Image.open("/tmp/screen.png")
+        text = pytesseract.image_to_string(screenshot)
+    elif(platform.system() == "Windows"):
+        pyautogui.screenshot()
+        text = pytesseract.image_to_string(screenshot)
 
     time.sleep(1)
     if 'Riffusion' in text:
